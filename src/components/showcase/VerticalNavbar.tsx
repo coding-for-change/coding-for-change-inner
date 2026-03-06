44648e9dc220dcb23331c70d8a5c00cae3ce19a0
jakoblandbrecht <jakob.landbrecht@tum.de>
2026-03-06 13:47:52 +0100

    change

--- src/components/showcase/VerticalNavbar.tsx
+++ src/components/showcase/VerticalNavbar.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from '../general';
-import { useLocation } from 'react-router';
+import { useLocation } from 'react-router-dom';
 
 export interface VerticalNavbarProps {}
 
